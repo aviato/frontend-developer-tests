3a4a67e2deb9fe7ea227fb9d0d3e62567f96279f
Ryan Price <rsprice90@gmail.com>
2021-08-23 17:38:21 -0700

    feat: add country helpers

---
+++ countries/src/utils/countries.ts
@@ -0,0 +1,41 @@
+import User from "../types/User";
+
+// Should use something like useMemo for these
+export function listUsersByCountry(users: User[]): Map<string, User[]> {
+  const countries: Map<string, User[]> = new Map([]);
+
+  users.forEach(user => {
+    const country = user.location.country;
+    const usersByCountry = countries.get(country)
+
+    if (usersByCountry) {
+      usersByCountry.push(user)
+    } else {
+      countries.set(country, [user]);
+    }
+  });
+
+  return countries;
+}
+
+export function getSortedCountries(users: User[], sortType: 'asc'|'desc') {
+  const countries = listUsersByCountry(users);
+
+  return new Map([...countries.entries()].sort((a, b) => {
+    if (sortType === 'asc') {
+      return a[1].length - b[1].length;
+    } else {
+      return b[1].length - a[1].length;
+    }
+  }))
+}
+
+export function getFlatCountriesList(countries: Map<string, User[]>) {
+  const result = [];
+
+  for (let key of countries.keys()) {
+    result.push(key);
+  }
+
+  return result;
+}
